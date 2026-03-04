75975af8a723ae65632e51cde2f45d407e52b93b
Kis Tamás <iscbst@inf.elte.hu>
2026-03-04 13:26:55 +0000

    Edit GameModel.cs

--- TransportTycoon/TransportTycoon.Model/GameModel.cs
+++ TransportTycoon/TransportTycoon.Model/GameModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,6 @@ namespace TransportTycoon.Model
 {
     public class GameModel
     {
+        Console.WriteLine("Hello World");
     }
 }
