f7df5731fc0739aa4ad9405ebe3cb30b0e70d201
Kis Tamás <iscbst@INF.ELTE.HU>
2026-03-04 19:42:58 +0100

    Load Created

---
+++ TransportTycoon/TransportTycoon.Model/Load.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTycoon.Model
+{
+    public enum LoadType 
+    {
+        Wheat,
+        Oil,
+        Wood,
+        Flour,
+        Rubber,
+        Paper,
+        People
+    }
+
+    public class Load
+    {
+        public int Price { protected get; set; }
+
+    }
+}
