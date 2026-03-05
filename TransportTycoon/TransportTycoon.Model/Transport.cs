0c4a1fc5fa8dfd3b65b1181ca4960099cb1b4934
Kocsmár Balázs <y3trse@INF.ELTE.HU>
2026-03-05 16:49:23 +0100

    Transport created

---
+++ TransportTycoon/TransportTycoon.Model/Transport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTycoon.Model
+{
+    public abstract class Transport : Vehicle
+    {
+        #region Field
+        public List<Goods> acceptedGoods {  get; protected set; }
+        #endregion      
+    }
+
+    public class Van : Transport
+    {
+        #region Constructor
+        public Van()
+        {
+
+        }
+        #endregion
+    }
+}
