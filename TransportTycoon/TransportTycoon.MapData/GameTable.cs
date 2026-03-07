7efb1e64ca2afba40acd48c7875e4cf6ea269fb7
Shiny <fenyes.adam02@gmail.com>
2026-03-07 12:14:34 +0100

    Basic GameTable class

---
+++ TransportTycoon/TransportTycoon.MapData/GameTable.cs
@@ -0,0 +1,53 @@
+﻿namespace TransportTycoon.MapData
+{
+    public class GameTable
+    {
+        #region Private constants
+        private const int DefaultWidth = 100;
+        private const int DefaultHeight = 100;
+        #endregion
+
+        #region Fields
+        public Field[,] Table { get; }
+        public int Width { get; }
+        public int Height { get; }
+
+        public List<(int, int)> Pointers { get; }
+        public List<(int, int)> BuildingIDs { get; }
+        #endregion
+
+        #region Constructors
+        public GameTable(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Table = new Field[width, height];
+            
+            Pointers = [];
+            BuildingIDs = [];
+
+            GenerateMap();
+        }
+
+        public GameTable() : this(DefaultWidth, DefaultHeight) { }
+        #endregion
+
+        #region Public methods
+        // TODO: Megcsinálni
+        public void CheckNeighboringTrees(int x, int y) { }
+        #endregion
+
+        #region Private methods
+        private void GenerateMap()
+        {
+            for (int i = 0; i < Table.GetLength(0); i++)
+            {
+                for (int j = 0; j < Table.GetLength(1); j++)
+                {
+                    Table[i, j] = new Plain(i, j, 0, 0);
+                }
+            }
+        }
+        #endregion
+    }
+}
