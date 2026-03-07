460b7e256c77d0d67b534d607bc7fc55fc8e64a9
Shiny <fenyes.adam02@gmail.com>
2026-03-07 16:34:31 +0100

    MainViewModel and RelayCommands

--- TransportTycoon/TransportTycoon.WPF/ViewModel/MainViewModel.cs
+++ TransportTycoon/TransportTycoon.WPF/ViewModel/MainViewModel.cs
@@ -1,4 +1,107 @@
-﻿namespace TransportTycoon.WPF.ViewModel
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using TransportTycoon.Model;
+
+namespace TransportTycoon.WPF.ViewModel
 {
-    public class MainViewModel : ViewModelBase { }
+    public class MainViewModel : ViewModelBase
+    {
+        #region Properties
+        #region Relay commands
+        public RelayCommand NewGameCommand { get; init; }
+        public RelayCommand ExitCommand { get; init; }
+
+        public RelayCommand NormalSpeedCommand { get; init; }
+        public RelayCommand FastSpeedCommand { get; init; }
+        public RelayCommand SuperFastSpeedCommand { get; init; }
+
+        public RelayCommand PauseGameCommand { get; init; }
+        public RelayCommand ResumeGameCommand { get; init; }
+        public RelayCommand EditorModeCommand { get; init; }
+        #endregion
+
+        public GameModel Model { get; init; }
+
+        public ObservableCollection<FieldViewModel> Tiles { get; init; }
+
+        public int Balance => Model.Balance;
+        public int GameTime => Model.GameTime;
+        public bool IsPaused => Model.Mode == GameMode.Paused;
+        public bool IsEditorMode => Model.Mode == GameMode.Editor;
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructors
+        public MainViewModel(GameModel model)
+        {
+            Model = model;
+
+            NewGameCommand = new(OnNewGame);
+            ExitCommand = new(OnExit);
+            NormalSpeedCommand = new(OnNormalSpeed);
+            FastSpeedCommand = new(OnFastSpeed);
+            SuperFastSpeedCommand = new(OnSuperFastSpeed);
+
+            PauseGameCommand = new(OnPauseGame);
+            ResumeGameCommand = new(OnResumeGame);
+            EditorModeCommand = new(OnEditorMode);
+
+            Tiles = [];
+        }
+        #endregion
+
+        #region Private methods
+        private void RefreshTable()
+        {
+
+        }
+        #endregion
+
+        #region Relay command methods
+        private void OnNewGame()
+        {
+            Model.NewGame();
+        }
+
+        private void OnExit()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnNormalSpeed()
+        {
+            Model.SetTimeSpeed(TimeSpeed.Normal);
+        }
+
+        private void OnFastSpeed()
+        {
+            Model.SetTimeSpeed(TimeSpeed.Fast);
+        }
+
+        private void OnSuperFastSpeed()
+        {
+            Model.SetTimeSpeed(TimeSpeed.SuperFast);
+        }
+
+        private void OnPauseGame()
+        {
+            Model.SetMode(GameMode.Paused);
+        }
+
+        private void OnResumeGame()
+        {
+            Model.SetMode(GameMode.Run);
+        }
+
+        private void OnEditorMode()
+        {
+            Model.SetMode(GameMode.Editor);
+        }
+        #endregion
+
+        #region Event methods
+        #endregion
+    }
 }
