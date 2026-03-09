42edd6b3780b4708727e948a9134cdcc76de755b
Kocsmár Balázs <y3trse@INF.ELTE.HU>
2026-03-09 19:33:25 +0100

    Stop setup

---
+++ TransportTycoon/TransportTycoon.MapData/Bridge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTycoon.MapData
+{
+    public abstract class Bridge : Infrastructure
+    {
+
+    }
+}
---
+++ TransportTycoon/TransportTycoon.MapData/Stop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTycoon.MapData
+{
+    public class Stop : Infrastructure
+    {
+        #region Fields
+        public List<BuildingBlocks>? Goods { get; private set; }
+        #endregion
+        #region Public Methods
+        public Stop()
+        {
+            //...
+        }
+        public List<Load> ShowLoads()
+        {
+            return null;//...
+        }
+        public bool VehicleToBuilding()
+        {
+            return false;//...
+        }
+        public bool BuildingToVehicle()
+        {
+            return false;//...
+        }
+        #endregion
+        #region Private Methods
+        private void SetBuildingBlocks()
+        {
+            //...
+        }
+        #endregion
+    }
+}
