09c19ec0faf00ee8d44bbc668c60b9dc75cdfd74
Kis Tamás <iscbst@INF.ELTE.HU>
2026-03-09 09:28:52 +0100

    FieldViewModel updated - Properties created

--- TransportTycoon/TransportTycoon.WPF/ViewModel/FieldViewModel.cs
+++ TransportTycoon/TransportTycoon.WPF/ViewModel/FieldViewModel.cs
@@ -1,4 +1,43 @@
-﻿namespace TransportTycoon.WPF.ViewModel
+﻿using TransportTycoon.MapData;
+
+namespace TransportTycoon.WPF.ViewModel
 {
-    public class FieldViewModel : ViewModelBase { }
+    public class FieldViewModel : ViewModelBase
+    {
+        #region Fields
+        private Field _field;
+        #endregion
+        #region Properties
+        public int X => _field.X;
+        public int Y => _field.Y;
+        public int Height => _field.Height;
+        public int TreeCounter
+        {
+            get
+            {
+                if (_field is Terrain terrain)
+                {
+                    return terrain.Trees;
+                }
+                else return 0;
+            }
+        }
+
+        #endregion
+        #region Constructor
+        public FieldViewModel(Field field)
+        {
+            _field = field;
+        }
+        #endregion
+        #region Public Methods
+        #endregion
+        #region Private Methods
+        #endregion
+        #region Private event Methods
+        #endregion
+
+
+
+    }
 }
