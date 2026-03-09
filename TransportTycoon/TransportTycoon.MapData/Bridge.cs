bba683665e731e951f2500348d199e592794f0fd
Kocsmár Balázs <y3trse@INF.ELTE.HU>
2026-03-09 19:46:01 +0100

    Bridge setup

--- TransportTycoon/TransportTycoon.MapData/Bridge.cs
+++ TransportTycoon/TransportTycoon.MapData/Bridge.cs
@@ -4,8 +4,43 @@ using System.Text;
 
 namespace TransportTycoon.MapData
 {
+    public enum BridgeType
+    {
+        HorizontalBlueBridge = 0, VerticalBlueBridge = 1, HorizontalYellowBridge = 2, VerticalYellowBridge = 3, HorizontalRedBridge = 4, VerticalRedBridge = 5
+    }
     public abstract class Bridge : Infrastructure
     {
-
+        #region Fields
+        public int SpeedLimit { get; private set; }
+        public int Range { get; private set; }
+        public BridgeType Type { get; private set; }
+        #endregion
+    }
+    public class YellowBridge : Bridge
+    {
+        #region Constructor
+        public YellowBridge()
+        {
+            //...
+        }
+        #endregion
+    }
+    public class BlueBridge : Bridge
+    {
+        #region Constructor
+        public BlueBridge()
+        {
+            //...
+        }
+        #endregion
+    }
+    public class RedBridge : Bridge
+    {
+        #region Constructor
+        public RedBridge()
+        {
+            //...
+        }
+        #endregion
     }
 }
