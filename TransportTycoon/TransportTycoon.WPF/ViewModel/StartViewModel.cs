5c7237697338dd415f1e8594fa50fe3c4ac86df0
Kis Tamás <iscbst@INF.ELTE.HU>
2026-03-11 13:25:17 +0100

    StartViewModel variables & commands inicialised

--- TransportTycoon/TransportTycoon.WPF/ViewModel/StartViewModel.cs
+++ TransportTycoon/TransportTycoon.WPF/ViewModel/StartViewModel.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TransportTycoon.Model;
 
 namespace TransportTycoon.WPF.ViewModel
 {
-    public class StartViewModel
+    public class StartViewModel : ViewModelBase
     {
+        //We need an interface for Dependecy Injection
+
+        public Difficulty SelectedDifficulty { get; set; }
+        public RelayCommand NewGameCommand { get; private set; }
+        public RelayCommand OpenGameCommand { get; private set; }
     }
 }
