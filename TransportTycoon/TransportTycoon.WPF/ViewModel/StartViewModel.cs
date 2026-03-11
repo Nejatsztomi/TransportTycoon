0b6e99dc2e46155cf50bc179c402f47f3c5e0e3a
Kis Tamás <iscbst@INF.ELTE.HU>
2026-03-11 14:05:20 +0100

    stucture created for StartViewModel

--- TransportTycoon/TransportTycoon.WPF/ViewModel/StartViewModel.cs
+++ TransportTycoon/TransportTycoon.WPF/ViewModel/StartViewModel.cs
@@ -8,10 +8,23 @@ namespace TransportTycoon.WPF.ViewModel
 {
     public class StartViewModel : ViewModelBase
     {
+        #region Fields
         //We need an interface for Dependecy Injection
-
         public Difficulty SelectedDifficulty { get; set; }
+        #endregion
+        #region Commands
         public RelayCommand NewGameCommand { get; private set; }
         public RelayCommand OpenGameCommand { get; private set; }
+        #endregion
+        #region Events
+
+        #endregion
+        #region Constructor
+        public StartViewModel() 
+        {
+
+        }
+        #endregion
+
     }
 }
