0869cd4825589cd963566fb4796babf36d030458
Shiny <fenyes.adam02@gmail.com>
2026-03-13 22:39:23 +0100

    Working Zoom

--- TransportTycoon/TransportTycoon.WPF/View/MainWindow.xaml.cs
+++ TransportTycoon/TransportTycoon.WPF/View/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TransportTycoon.WPF.ViewModel;
 
 namespace TransportTycoon.WPF
 {
@@ -16,9 +17,35 @@ namespace TransportTycoon.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructors
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Private event methods
+        private void MapScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            // Zoom with touchpad: Pinch-to-zoom
+            // Zoom with mouse: Ctrl+Scroll wheel
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                e.Handled = true;
+
+                if (this.DataContext is MainViewModel vm)
+                {
+                    if (e.Delta > 0 && vm.ZoomLevel < 3.0)
+                    {
+                        vm.ZoomLevel += 0.1;
+                    }
+                    else if (e.Delta < 0 && vm.ZoomLevel > 0.4)
+                    {
+                        vm.ZoomLevel -= 0.1;
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
