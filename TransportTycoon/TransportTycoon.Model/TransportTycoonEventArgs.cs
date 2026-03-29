394f56d0b95542dc26fb4e6b9a7d34229e013ac9
Kis Tamás <iscbst@INF.ELTE.HU>
2026-03-29 17:54:47 +0200

    extended TransportTycoonEventArgs with Balance and Maintance

--- TransportTycoon/TransportTycoon.Model/TransportTycoonEventArgs.cs
+++ TransportTycoon/TransportTycoon.Model/TransportTycoonEventArgs.cs
@@ -1,9 +1,13 @@
 ﻿namespace TransportTycoon.Model
 {
-    public class TransportTycoonEventArgs(int gameTime, int numberOfVehicles) : EventArgs
+    public class TransportTycoonEventArgs(int gameTime, int numberOfVehicles, int maintance, int balance) : EventArgs
     {
         #region Properties
         public int GameTime => gameTime;
+
+        public int Maintance => maintance;
+        
+        public int Balance => balance;
 
         public int NumberOfVehicles => numberOfVehicles;
         #endregion
