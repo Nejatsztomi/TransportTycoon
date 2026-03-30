02d52d8de16e4d28012988c7445269ad0e1023b7
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-03-30 13:40:10 +0200

    Basic test strucutre
    Created the test strucutre. Create the skeleton of the GameModel tests.

---
+++ TransportTycoon/TransportTycoon.Test/Model/GameModelTest.cs
@@ -0,0 +1,82 @@
+using TransportTycoon.Model;
+
+namespace TransportTycoon.Test.Model;
+
+public class GameModelTest
+{
+    [TestClass]
+    public class ConstructorTest
+    {
+        [TestMethod]
+        public void Constructor_WithAllParameters() { }
+
+        [TestMethod]
+        public void Constructor_WithBalance() { }
+
+        [TestMethod]
+        public void Constructor_WithDifficulty() { }
+    }
+
+    public class EventTest
+    {
+        [TestClass]
+        public class EventRaisedTest
+        {
+            private static GameModel _gameModel;
+
+            [ClassInitialize]
+            public static void Initialize(TestContext context)
+            {
+                _gameModel = new GameModel(Difficulty.Medium, 1000, null);
+            }
+
+            [TestMethod]
+            public void NewGameCreated_EventIsRaised() { }
+
+            [TestMethod]
+            public void GameModeChanged_EventIsRaised() { }
+
+            [TestMethod]
+            public void TimeSpeedChanged_EventIsRaised() { }
+
+            [TestMethod]
+            public void GameOver_EventIsRaised() { }
+
+            [TestMethod]
+            public void GameTicked_EventIsRaised() { }
+
+            [TestMethod]
+            public void GameAdvanced_EventIsRaised() { }
+
+            [TestMethod]
+            public void InfrastructureBuilt_EventIsRaised() { }
+        }
+
+        [TestClass]
+        public class EventArgumentTest
+        {
+            private static GameModel _gameModel;
+
+            [ClassInitialize]
+            public static void Initialize(TestContext context)
+            {
+                _gameModel = new GameModel(Difficulty.Medium, 1000, null);
+            }
+
+            [TestMethod]
+            public void GameModeChanged_EventArgumentIsCorrect() { }
+
+            [TestMethod]
+            public void TimeSpeedChanged_EventArgumentIsCorrect() { }
+
+            [TestMethod]
+            public void GameOver_EventArgumentIsCorrect() { }
+
+            [TestMethod]
+            public void GameAdvanced_EventArgumentIsCorrect() { }
+
+            [TestMethod]
+            public void InfrastructureBuilt_EventArgumentIsCorrect() { }
+        }
+    }
+}
