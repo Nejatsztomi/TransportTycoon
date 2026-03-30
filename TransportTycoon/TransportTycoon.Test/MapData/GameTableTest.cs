7d4080c5720163f2fd3ceeb1f8d3efce374c6909
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-03-30 14:50:58 +0200

    Added skeleton for GameTable tests.
    Added tests for constructor, map generation and tree generation.

---
+++ TransportTycoon/TransportTycoon.Test/MapData/GameTableTest.cs
@@ -0,0 +1,40 @@
+using TransportTycoon.MapData;
+
+namespace TransportTycoon.Test.MapData;
+
+public class GameTableTest
+{
+    [TestClass]
+    public class ConstructorTest
+    {
+        [TestMethod]
+        public void Constructor_WithAllParameters() { }
+
+        [TestMethod]
+        public void Constructor_Default() { }
+    }
+
+    [TestClass]
+    public class MapGenerationTest
+    {
+        private static GameTable _gameTable = null!;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _gameTable = new GameTable(10, 10);
+        }
+
+        [TestMethod]
+        public void GenerateMap_MapIsGenerated() { }
+
+        [TestMethod]
+        public void GenerateMap_MapHasValidFields() { }
+
+        [TestMethod]
+        public void GenerateMap_MapHasTrees() { }
+
+        [TestMethod]
+        public void GenerateMap_MapHasValidTrees() { }
+    }
+}
