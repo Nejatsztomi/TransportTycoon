86a011d9276194708e3c51bf426e6c5e881e2aab
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-03-30 20:00:27 +0200

    Added tests for event args.

--- TransportTycoon/TransportTycoon.Test/Model/GameModelTest.cs
+++ TransportTycoon/TransportTycoon.Test/Model/GameModelTest.cs
@@ -214,26 +214,109 @@ public class GameModelTest
         [TestClass]
         public class EventArgumentTest
         {
-            private static GameModel _gameModel = null!;
-
-            [ClassInitialize]
-            public static void Initialize(TestContext _)
+            private static IEnumerable<object[]> GetAllGameModes()
             {
-                var mockTimer = Substitute.For<ITimer>();
-                _gameModel = new GameModel(Difficulty.Medium, 1000, mockTimer);
+                foreach (var role in Enum.GetValues<GameMode>())
+                {
+                    yield return new object[] { role };
+                }
+            }
+
+            private static IEnumerable<object[]> GetAllTimeSpeeds()
+            {
+                foreach (var role in Enum.GetValues<TimeSpeed>())
+                {
+                    yield return new object[] { role };
+                }
+            }
+
+            private static GameModel _gameModel = null!;
+            private static ITimer _mockTimer = null!;
+
+            [TestInitialize]
+            public void Initialize()
+            {
+                _mockTimer = Substitute.For<ITimer>();
+                _gameModel = new GameModel(Difficulty.Medium, 1000, _mockTimer);
             }
 
             [TestMethod]
-            public void GameModeChanged_EventArgumentIsCorrect() { }
+            [DynamicData(nameof(GetAllGameModes))]
+            public void GameModeChanged_EventArgumentIsCorrect(GameMode expectedGameMode)
+            {
+                GameMode actualGameMode = GameMode.Run;
+
+                EventHandler<GameMode> handler = (_, e) =>
+                {
+                    actualGameMode = e;
+                };
+
+                try
+                {
+                    _gameModel.GameModeChanged += handler;
+                    _gameModel.SetMode(expectedGameMode);
+                    Assert.AreEqual(expectedGameMode, actualGameMode, "GameModeChanged event should have correct argument");
+                }
+                finally
+                {
+                    _gameModel.GameModeChanged -= handler;
+                }
+            }
 
             [TestMethod]
-            public void TimeSpeedChanged_EventArgumentIsCorrect() { }
+            [DynamicData(nameof(GetAllTimeSpeeds))]
+            public void TimeSpeedChanged_EventArgumentIsCorrect(TimeSpeed expectedTimeSpeed)
+            {
+                TimeSpeed actualTimeSpeed = TimeSpeed.Normal;
+
+                EventHandler<TimeSpeed> handler = (_, e) =>
+                {
+                    actualTimeSpeed = e;
+                };
+
+                try
+                {
+                    _gameModel.TimeSpeedChanged += handler;
+                    _gameModel.SetTimeSpeed(expectedTimeSpeed);
+                    Assert.AreEqual(expectedTimeSpeed, actualTimeSpeed, "TimeSpeedChanged event should have correct argument");
+                }
+                finally
+                {
+                    _gameModel.TimeSpeedChanged -= handler;
+                }
+            }
 
             [TestMethod]
             public void GameOver_EventArgumentIsCorrect() { }
 
             [TestMethod]
-            public void GameAdvanced_EventArgumentIsCorrect() { }
+            public void GameAdvanced_EventArgumentIsCorrect()
+            {
+                List<Tuple<int, int>> actualTrees = [];
+
+                EventHandler<List<Tuple<int, int>>> handler = (_, e) =>
+                {
+                    actualTrees = e;
+                };
+
+                try
+                {
+
+                    _gameModel.GameAdvanced += handler;
+                    // Indítsunk egy új játékot, hogy biztosan legyen mapunk és fáink
+                    _gameModel.NewGame();
+                    // Szimuláljuk a timer tick eseményét 10x (egyelőre ennyi kell egy event kiváltáshoz)
+                    for (int i = 0; i < 10; i++)
+                    {
+                        _mockTimer.Elapsed += Raise.EventWith(this, EventArgs.Empty);
+                    }
+                    Assert.IsNotEmpty(actualTrees, "GameAdvanced event after 10 timer ticks should raise and return with non-empty trees changed");
+                }
+                finally
+                {
+                    _gameModel.GameAdvanced -= handler;
+                }
+            }
 
             [TestMethod]
             public void InfrastructureBuilt_EventArgumentIsCorrect() { }
