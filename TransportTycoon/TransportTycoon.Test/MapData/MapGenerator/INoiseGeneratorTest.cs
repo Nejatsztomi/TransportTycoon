6614eb481027a169faad7f36dd18145181ca1261
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-03-30 14:48:02 +0200

    Added skeleton for INoiseGenerator.
    Added skeleton for testing the PerlinNoiseGenerator class and it's factory

---
+++ TransportTycoon/TransportTycoon.Test/MapData/MapGenerator/INoiseGeneratorTest.cs
@@ -0,0 +1,34 @@
+using TransportTycoon.MapData.MapGenerator;
+
+namespace TransportTycoon.Test.MapData.MapGenerator;
+
+public class INoiseGeneratorTest
+{
+    [TestClass]
+    public class FactoryCreateTest
+    {
+        [TestMethod]
+        public void PerlinNoiseFactoryCreate_WithAllParameters() { }
+    }
+
+    public class NoiseGenerationTest
+    {
+        [TestClass]
+        public class PerlinNoiseGeneratorTest
+        {
+            private static INoiseGenerator _generator = null!;
+
+            [ClassInitialize]
+            public static void Initialize(TestContext testContext)
+            {
+                _generator = PerlinNoiseGeneratorFactory.Create(10, 10, 42);
+            }
+
+            [TestMethod]
+            public void PerlinNoiseGenerate_IsCorrectSize() { }
+
+            [TestMethod]
+            public void PerlinNoiseGenerate_ValuesAreBetweenZeroAndOne() { }
+        }
+    }
+}
