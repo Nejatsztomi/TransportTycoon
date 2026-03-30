94455af8e676d503f175ea19655f6c109464be40
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-03-30 20:48:23 +0200

    Added tests for generating Perlin Noise

--- TransportTycoon/TransportTycoon.Test/MapData/MapGenerator/INoiseGeneratorTest.cs
+++ TransportTycoon/TransportTycoon.Test/MapData/MapGenerator/INoiseGeneratorTest.cs
@@ -21,19 +21,45 @@ public class INoiseGeneratorTest
         [TestClass]
         public class PerlinNoiseGeneratorTest
         {
+            private static readonly int _width = 10;
+            private static readonly int _height = 15;
             private static INoiseGenerator _generator = null!;
 
             [ClassInitialize]
             public static void Initialize(TestContext _)
             {
-                _generator = PerlinNoiseGeneratorFactory.Create(10, 10, 42);
+                _generator = PerlinNoiseGeneratorFactory.Create(_width, _height, 42);
             }
 
             [TestMethod]
-            public void PerlinNoiseGenerate_IsCorrectSize() { }
+            public void PerlinNoiseGenerate_IsCorrectSize()
+            {
+                float[,] noise = _generator.GenerateNoise(0.1f);
+
+                Assert.AreEqual(_width, noise.GetLength(0), "Generated noise width should be what is in the constructor");
+                Assert.AreEqual(_height, noise.GetLength(1), "Generated noise height should be what is in the constructor");
+            }
 
             [TestMethod]
-            public void PerlinNoiseGenerate_ValuesAreBetweenZeroAndOne() { }
+            [DataRow(0.1f)]
+            [DataRow(0.5f)]
+            [DataRow(0.9f)]
+            public void PerlinNoiseGenerate_ValuesAreBetweenZeroAndOne(float noiseScale)
+            {
+                float[,] noise = _generator.GenerateNoise(noiseScale);
+
+                bool hasValuesOutsideRange = false;
+                foreach (var value in noise)
+                {
+                    if (value < 0.0f || value > 1.0f)
+                    {
+                        hasValuesOutsideRange = true;
+                        break;
+                    }
+                }
+
+                Assert.IsFalse(hasValuesOutsideRange);
+            }
         }
     }
 }
