17b14558b45e66fed922068433c955f3b282ae3c
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-03-30 20:30:54 +0200

    Added basic tests for map generation.
    Check for valid tiles and trees.

--- TransportTycoon/TransportTycoon.Test/MapData/GameTableTest.cs
+++ TransportTycoon/TransportTycoon.Test/MapData/GameTableTest.cs
@@ -23,7 +23,7 @@ public class GameTableTest
 
             Assert.IsNotNull(gameTable.Table, "GameTable should initialize the Table array");
             Assert.AreEqual(width, gameTable.Table.GetLength(0), "GameTable's Table should have the correct width");
-            Assert.AreEqual(height, gameTable.Table.GetLength(1), "GameTable's Table should have the correct height")
+            Assert.AreEqual(height, gameTable.Table.GetLength(1), "GameTable's Table should have the correct height");
         }
 
         [TestMethod]
@@ -39,24 +39,61 @@ public class GameTableTest
     [TestClass]
     public class MapGenerationTest
     {
-        private static GameTable _gameTable = null!;
+        private GameTable _gameTable = null!;
 
-        [ClassInitialize]
-        public static void Initialize(TestContext _)
+        [TestInitialize]
+        public void Initialize()
         {
-            _gameTable = new GameTable(10, 10);
+            _gameTable = new(5, 5);
         }
 
         [TestMethod]
-        public void GenerateMap_MapIsGenerated() { }
+        public void GenerateMap_MapIsGenerated()
+        {
+            _gameTable.GenerateMap();
+            Assert.IsNotEmpty(_gameTable.Table, "GameTable should generate a non-empty map");
+
+            foreach (var field in _gameTable.Table)
+            {
+                Assert.IsNotNull(field, "Each field in the map should be initialized");
+                Assert.IsTrue(field is Water || field is Terrain);
+            }
+        }
 
         [TestMethod]
-        public void GenerateMap_MapHasValidFields() { }
+        public void GenerateMap_MapHasValidFields()
+        {
+            _gameTable.GenerateMap();
+            foreach (var field in _gameTable.Table)
+            {
+                Assert.IsTrue(0 <= field.Height && field.Height <= 4, "Each field should have a height between 0 and 4");
+                Assert.IsTrue(_gameTable.IsTileHeightPossible(field.X, field.Y, field.Height), "Each field should have a possible height");
+            }
+        }
 
         [TestMethod]
-        public void GenerateMap_MapHasTrees() { }
+        public void GenerateMap_MapHasTrees()
+        {
+            _gameTable.GenerateMap();
+            int treeCount = 0;
+
+            foreach (var field in _gameTable.Table)
+            {
+                treeCount += field.GetTrees();
+            }
+
+            Assert.IsGreaterThan(0, treeCount, "Generated map should contain trees");
+        }
 
         [TestMethod]
-        public void GenerateMap_MapHasValidTrees() { }
+        public void GenerateMap_MapHasValidTrees()
+        {
+            _gameTable.GenerateMap();
+
+            foreach (var field in _gameTable.Table)
+            {
+                Assert.IsTrue(0 <= field.GetTrees() && field.GetTrees() <= 4, "Each field should have a trees between 0 and 4");
+            }
+        }
     }
 }
