b54872ebf79ddceb3ee1ee089bfd0b1a506ec3af
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-01 21:47:09 +0200

    Implemented new map generator

--- TransportTycoon/TransportTycoon.MapData/MapGenerator/MapGenerator.cs
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/MapGenerator.cs
@@ -1,7 +1,20 @@
-﻿using TransportTycoon.MapData.MapGenerator.TerrainGeneration;
+﻿using TransportTycoon.MapData.MapGenerator.NoiseGenerator;
+using TransportTycoon.MapData.MapGenerator.TerrainGeneration;
 
 namespace TransportTycoon.MapData.MapGenerator
 {
+    public static class MapGeneratorFactory
+    {
+        public static IMapGenerator CreateMapGenerator(MapGenerationSettings settings)
+        {
+            INoiseGenerator noiseGenerator = new PerlinNoiseGenerator();
+            ITerrainGenerator terrainGenerator = new TerrainGenerator(noiseGenerator);
+            IForestGenerator forestGenerator = new ForestGenerator(noiseGenerator);
+            IWaterGenerator waterGenerator = new WaterGenerator(noiseGenerator);
+            return new MapGenerator(settings, terrainGenerator, forestGenerator, waterGenerator);
+        }
+    }
+
     internal class MapGenerator : IMapGenerator
     {
         #region Properties
@@ -22,7 +35,33 @@ namespace TransportTycoon.MapData.MapGenerator
         #endregion
 
         #region Public methods
-        public Field[,] GenerateMap(MapGenerationContext context) { }
+        public Field[,] GenerateMap(MapGenerationContext context)
+        {
+            int[,] heightMap = TerrainGenerator.GenerateTerrain(Settings.Biome, context);
+            int[,] forestMap = ForestGenerator.GenerateForests(heightMap, context);
+            bool[,] waterMap = WaterGenerator.GenerateWaterMap(Settings.RiverCount, heightMap, context);
+
+            Field[,] map = new Field[context.Width, context.Height];
+            for (int x = 0; x < context.Width; x++)
+            {
+                for (int y = 0; y < context.Height; y++)
+                {
+                    if (waterMap[x, y])
+                    {
+                        map[x, y] = new Water(x, y);
+                    }
+                    else
+                    {
+                        Terrain terrain = new(x, y, heightMap[x, y])
+                        {
+                            Trees = forestMap[x, y]
+                        };
+                        map[x, y] = terrain;
+                    }
+                }
+            }
+            return map;
+        }
         #endregion
     }
 }
