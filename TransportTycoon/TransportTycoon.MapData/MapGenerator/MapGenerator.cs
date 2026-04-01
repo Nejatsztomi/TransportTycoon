a976a255fb371cf8011685697b877668fa53e180
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-01 21:05:51 +0200

    Added basic structure for MapGenerator
    Implented the interface and added helper generator dependencies

---
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/MapGenerator.cs
@@ -0,0 +1,28 @@
+﻿using TransportTycoon.MapData.MapGenerator.TerrainGeneration;
+
+namespace TransportTycoon.MapData.MapGenerator
+{
+    internal class MapGenerator : IMapGenerator
+    {
+        #region Properties
+        private MapGenerationSettings Settings { get; }
+        private ITerrainGenerator TerrainGenerator { get; }
+        private IForestGenerator ForestGenerator { get; }
+        private IWaterGenerator WaterGenerator { get; }
+        #endregion
+
+        #region Constructors
+        public MapGenerator(MapGenerationSettings settings, ITerrainGenerator terrainGenerator, IForestGenerator forestGenerator, IWaterGenerator waterGenerator)
+        {
+            Settings = settings;
+            TerrainGenerator = terrainGenerator;
+            ForestGenerator = forestGenerator;
+            WaterGenerator = waterGenerator;
+        }
+        #endregion
+
+        #region Public methods
+        public Field[,] GenerateMap(MapGenerationContext context) { }
+        #endregion
+    }
+}
