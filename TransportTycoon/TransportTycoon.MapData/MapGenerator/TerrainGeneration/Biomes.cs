dd766f7fbe7365ab434a1691ea0e3e6b0e109252
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-01 21:01:52 +0200

    Added default biome based on prevoius implementation

---
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/TerrainGeneration/Biomes.cs
@@ -0,0 +1,15 @@
+﻿namespace TransportTycoon.MapData.MapGenerator.TerrainGeneration
+{
+    public static class Biomes
+    {
+        public static IBiome Default { get; } = new DefaultBiome();
+    }
+
+    internal class DefaultBiome : IBiome
+    {
+        public float PlainRange => 0.55f;
+        public float HillRange => 0.75f;
+        public float MountainRange => 0.95f;
+        public float HighMountainRange => 1f;
+    }
+}
