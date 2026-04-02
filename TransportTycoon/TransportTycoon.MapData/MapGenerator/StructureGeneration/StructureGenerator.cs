7642006b7cad75834a4e6abd8b246ff5b2a368bd
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-02 16:55:51 +0200

    Added StructureGenerator class and implemented the interface

---
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/StructureGeneration/StructureGenerator.cs
@@ -0,0 +1,33 @@
+﻿namespace TransportTycoon.MapData.MapGenerator.StructureGeneration
+{
+    public static class StructureGeneratorFactory
+    {
+        public static IStructureGenerator Create(ICityGenerator cityGenerator) => new StructureGenerator(cityGenerator);
+    }
+
+    internal class StructureGenerator : IStructureGenerator
+    {
+        #region Properties
+        private ICityGenerator CityGenerator { get; }
+        #endregion
+
+        #region Constructors
+        public StructureGenerator(ICityGenerator cityGenerator)
+        {
+            CityGenerator = cityGenerator;
+        }
+        #endregion
+
+        #region Public methods
+        public SavedEntity? TryPlace(int[,] heightMap, bool[,] waterMap, bool[,] structureMap, string type, int x, int y, int radius, MapGenerationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SavedEntity ForcePlace(int[,] heightMap, bool[,] waterMap, bool[,] structureMap, string type, int x, int y, int radius, MapGenerationContext context)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
