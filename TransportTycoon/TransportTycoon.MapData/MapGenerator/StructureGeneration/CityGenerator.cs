f468b1a72c1b762a561bbb7d41f8ab6b35234db0
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-02 16:55:34 +0200

    Added CityGenerator class and implemented the interface

---
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/StructureGeneration/CityGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace TransportTycoon.MapData.MapGenerator.StructureGeneration
+{
+    public static class CityGeneratorFactory
+    {
+        public static ICityGenerator Create() => new CityGenerator();
+    }
+
+    internal class CityGenerator : ICityGenerator
+    {
+        #region Constructor
+        public CityGenerator() { }
+        #endregion
+
+        #region Public methods
+        public SavedEntity GenerateCity(SavedEntity city, MapGenerationContext context)
+        {
+            return city;
+        }
+        #endregion
+    }
+}
