2848fc1f9a2fdc8e954638f824bab5ce9ebb8ec3
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-02 11:33:51 +0200

    Added TODO comment

--- TransportTycoon/TransportTycoon.MapData/MapGenerator/MapGenerationContext.cs
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/MapGenerationContext.cs
@@ -1,4 +1,5 @@
 ﻿namespace TransportTycoon.MapData.MapGenerator
 {
-    public readonly record struct MapGenerationContext(int Height, int Width, int Seed);
+    // TODO: Add invarints to ensure width and height are positive, and seed can be any integer
+    public readonly record struct MapGenerationContext(int Width, int Height, int Seed);
 }
