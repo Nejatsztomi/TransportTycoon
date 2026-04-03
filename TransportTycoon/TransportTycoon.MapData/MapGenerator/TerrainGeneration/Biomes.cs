54c29091173bb7bd72f5335bd67a1b16477426a1
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-03 17:58:06 +0200

    Added WaterRange

--- TransportTycoon/TransportTycoon.MapData/MapGenerator/TerrainGeneration/Biomes.cs
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/TerrainGeneration/Biomes.cs
@@ -7,6 +7,7 @@
 
     internal class DefaultBiome : IBiome
     {
+        public float WaterRange => 0.3f;
         public float PlainRange => 0.55f;
         public float HillRange => 0.75f;
         public float MountainRange => 0.95f;
