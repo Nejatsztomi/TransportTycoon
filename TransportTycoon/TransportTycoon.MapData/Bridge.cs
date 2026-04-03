4552cbc35006377267a5028593b91742b4d06fb4
Kocsmár Balázs <y3trse@INF.ELTE.HU>
2026-04-03 12:14:13 +0200

    Formating

--- TransportTycoon/TransportTycoon.MapData/Bridge.cs
+++ TransportTycoon/TransportTycoon.MapData/Bridge.cs
@@ -4,7 +4,7 @@ namespace TransportTycoon.MapData
 {
     public enum BridgeType
     {
-        HorizontalGreenBridge = 0, VerticalGreenBridge = 1, HorizontalYellowBridge = 2, VerticalYellowBridge = 3, HorizontalRedBridge = 4, VerticalRedBridge = 5, Null=6
+        HorizontalGreenBridge = 0, VerticalGreenBridge = 1, HorizontalYellowBridge = 2, VerticalYellowBridge = 3, HorizontalRedBridge = 4, VerticalRedBridge = 5, Null = 6
     }
 
     public abstract class Bridge : Infrastructure
@@ -28,7 +28,7 @@ namespace TransportTycoon.MapData
             FieldType = FieldType.Bridge;
             BridgeType = type;
             Height = height;
-            Price = 50;;
+            Price = 50;
         }
         #endregion
     }
