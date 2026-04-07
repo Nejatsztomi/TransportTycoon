0c26fa0e0ba5bebc4c4c09426d656fa5ab3c8b0a
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-07 18:25:28 +0200

    Added the Vertex class

---
+++ TransportTycoon/TransportTycoon.Model/Graph/Vertex.cs
@@ -0,0 +1,23 @@
+﻿using TransportTycoon.MapData;
+
+namespace TransportTycoon.Model.Graph
+{
+    public class Vertex
+    {
+        #region Properties
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public FieldType Type { get; private set; }
+        public bool IsValidDestination => Type == FieldType.Stop;
+        #endregion
+
+        #region Constructors
+        public Vertex(int x, int y, FieldType type)
+        {
+            X = x;
+            Y = y;
+            Type = type;
+        }
+        #endregion
+    }
+}
