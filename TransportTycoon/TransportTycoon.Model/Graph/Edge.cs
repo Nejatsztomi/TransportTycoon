23c47331374d63b7a344305b88827fd430ac9f18
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-07 18:33:38 +0200

    Added the Edge class

---
+++ TransportTycoon/TransportTycoon.Model/Graph/Edge.cs
@@ -0,0 +1,51 @@
+﻿using TransportTycoon.MapData;
+
+namespace TransportTycoon.Model.Graph
+{
+    public class Edge
+    {
+        #region Public constants
+        /// <summary>
+        /// A cost modifier for calculating the cost of an edge.
+        /// </summary>
+        /// <remarks>
+        /// This will likely to change in the future for a more dynamic one.
+        /// Which includes bridge speed limits and terrain heights.
+        /// </remarks>
+        public const double CostModifier = 2.0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The edge's starting vertex.
+        /// </summary>
+        public Vertex StartVertex { get; private set; }
+        /// <summary>
+        /// The edge's ending vertex.
+        /// </summary>
+        public Vertex EndVertex { get; private set; }
+        /// <summary>
+        /// List of the road tiles between the <see cref="StartVertex"/> and <see cref="EndVertex"/>.
+        /// Also including the <see cref="StartVertex"/> and <see cref="EndVertex"/>.
+        /// It is ordered from <see cref="StartVertex"/> to <see cref="EndVertex"/>.
+        /// </summary>
+        public List<Field> Roads { get; private set; }
+        /// <summary>
+        /// Gives the cost of the edge.
+        /// </summary>
+        /// <remarks>
+        /// This is calculated my multiplying the number of road tiles with the <see cref="CostModifier"/>.
+        /// </remarks>
+        public double Cost => Roads.Count * CostModifier;
+        #endregion
+
+        #region Constructors
+        public Edge(Vertex startVertex, Vertex endVertex, List<Field> roads)
+        {
+            StartVertex = startVertex;
+            EndVertex = endVertex;
+            Roads = roads;
+        }
+        #endregion
+    }
+}
