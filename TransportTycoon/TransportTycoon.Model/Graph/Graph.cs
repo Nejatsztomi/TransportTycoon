d013a932203d082489cf35dfd0cdb67f271fbe0f
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-10 14:38:54 +0200

    Created Graph class

---
+++ TransportTycoon/TransportTycoon.Model/Graph/Graph.cs
@@ -0,0 +1,16 @@
+﻿namespace TransportTycoon.Model.Graph
+{
+    public class Graph
+    {
+        #region Properties
+        public Dictionary<Node, List<Edge>> AdjacencyList { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Graph(Dictionary<Node, List<Edge>> adjacencyList)
+        {
+            AdjacencyList = adjacencyList;
+        }
+        #endregion
+    }
+}
