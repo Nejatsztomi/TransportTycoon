69811f99286e801093083b99c483028e2857f83a
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-10 14:42:20 +0200

    Created GraphBuilder class

---
+++ TransportTycoon/TransportTycoon.Model/Graph/GraphBuilder.cs
@@ -0,0 +1,22 @@
+﻿using TransportTycoon.MapData;
+
+namespace TransportTycoon.Model.Graph
+{
+    public class GraphBuilder
+    {
+        #region Private fields
+        private HashSet<Field> _visitedFields;
+        #endregion
+
+        #region Constructors
+        public GraphBuilder()
+        {
+            _visitedFields = [];
+        }
+        #endregion
+
+        #region Public methods
+        // public Graph BuildGraph(GameTable table) { }
+        #endregion
+    }
+}
