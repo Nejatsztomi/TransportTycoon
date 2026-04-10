3772cb78c65f60fb9a8982bb1a2530afd9cfb16e
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-10 15:00:58 +0200

    Created SharedRoadSequence for storing the road tiles in sequence

---
+++ TransportTycoon/TransportTycoon.Model/Graph/SharedRoadSequence.cs
@@ -0,0 +1,55 @@
+﻿using TransportTycoon.MapData;
+
+namespace TransportTycoon.Model.Graph
+{
+    /// <summary>
+    /// A shared road sequence between two edges in the graph.
+    /// It is sequenced in the order of the starting edge to the finishing edge.
+    /// </summary>
+    public class SharedRoadSequence
+    {
+        #region Private fields
+        /// <summary>
+        /// The underlying data.
+        /// </summary>
+        private readonly Field[] _fields;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of this class with the specified collection of fields.
+        /// </summary>
+        /// <param name="fields">The list of fields to include in the sequence.</param>
+        public SharedRoadSequence(List<Field> fields)
+        {
+            _fields = [.. fields];
+        }
+        #endregion
+
+        #region Enumerators
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection of fields in forward order.
+        /// </summary>
+        /// <returns>An enumerator for the collection of fields, starting from the first field and proceeding in order to the last field.</returns>
+        public IEnumerator<Field> ForwardEnumerator()
+        {
+            for (int i = 0; i < _fields.Length; i++)
+            {
+                yield return _fields[i];
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection of fields in reversed order.
+        /// </summary>
+        /// <returns>An enumerator for the collection of fields, starting from the last field and proceeding in reverse order to the starting field.</returns>
+        public IEnumerator<Field> BackwardEnumerator()
+        {
+            for (int i = _fields.Length - 1; i >= 0; i--)
+            {
+                yield return _fields[i];
+            }
+        }
+        #endregion
+    }
+}
