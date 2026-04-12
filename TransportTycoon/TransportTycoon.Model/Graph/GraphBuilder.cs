644960b954537294beedeeba8805abe226604a15
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-12 13:15:08 +0200

    Added graph building algorithm
    This algorithm takes the GameTable's map and uses Walkers to traverse it

--- TransportTycoon/TransportTycoon.Model/Graph/GraphBuilder.cs
+++ TransportTycoon/TransportTycoon.Model/Graph/GraphBuilder.cs
@@ -7,22 +7,55 @@ namespace TransportTycoon.Model.Graph
     /// </summary>
     public class GraphBuilder
     {
-        #region Private fields
-        /// <summary>
-        /// A global, shared set of visited fields for the entire graph building process.
-        /// </summary>
-        private HashSet<Field> _visitedFields;
-        #endregion
-
         #region Constructors
-        public GraphBuilder()
-        {
-            _visitedFields = [];
-        }
+        public GraphBuilder() { }
         #endregion
 
         #region Public methods
-        // public Graph BuildGraph(GameTable table) { }
+        public Graph BuildGraph(GameTable table)
+        {
+            Queue<Walker> walkersQ = [];
+            List<Edge> edges = [];
+            List<Node> nodes = [];
+            HashSet<(int X, int Y)> visitedFields = [];
+
+            for (int x = 0; x < table.Width; x++)
+            {
+                for (int y = 0; y < table.Height; y++)
+                {
+                    if (table[x, y].FieldType == FieldType.Stop && !visitedFields.Contains((x, y)))
+                    {
+                        Node startNode = new(x, y, table[x, y].FieldType);
+                        nodes.Add(startNode);
+                        Walker initialWalker = new(startNode, table, visitedFields);
+                        visitedFields.Add((x, y));
+                        walkersQ.Enqueue(initialWalker);
+                    }
+
+                    while (walkersQ.Count > 0)
+                    {
+                        Walker walker = walkersQ.Dequeue();
+                        WalkerResult result = walker.Walk();
+
+                        // Add the created edges to the graph
+                        edges.AddRange(result.CreatedEdges);
+
+                        if (result.EndNode is not null)
+                        {
+                            nodes.Add(result.EndNode);
+                        }
+
+                        // Add the new walkers to the queue for further exploration
+                        foreach (Walker newWalker in result.NewWalkers)
+                        {
+                            walkersQ.Enqueue(newWalker);
+                        }
+                    }
+                }
+            }
+
+            return new Graph(nodes, edges);
+        }
         #endregion
     }
 }
---
+++ TransportTycoon/TransportTycoon.Model/Graph/Walker.cs
@@ -0,0 +1,148 @@
+﻿using TransportTycoon.MapData;
+
+namespace TransportTycoon.Model.Graph
+{
+    public record WalkerResult(
+    List<Edge> CreatedEdges,
+    List<Walker> NewWalkers,
+    Node? EndNode
+    );
+
+    public class Walker
+    {
+        #region Private fields
+        private readonly Node _startNode;
+        private readonly HashSet<(int X, int Y)> _visitedFields;
+        private readonly List<Field> _roads;
+        private readonly GameTable _gameTable;
+        #endregion
+
+        #region Constructors
+        public Walker(Node startNode, GameTable gameTable, HashSet<(int X, int Y)> visitedFields)
+        {
+            _startNode = startNode;
+            _gameTable = gameTable;
+            _visitedFields = visitedFields;
+            _roads = [];
+        }
+        #endregion
+
+        #region Public methods
+        public WalkerResult Walk()
+        {
+            (int dirx, int diry)[] directions = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+            Node? terminatingNode = null;
+            Node currentNode = _startNode;
+            (double forwardCost, double backwardCost) = (0.0, 0.0);
+
+            while (terminatingNode is null)
+            {
+                Field? nextField = null;
+                foreach ((int dirx, int diry) in directions)
+                {
+                    nextField = GetNextValidField(currentNode.X + dirx, currentNode.Y + diry);
+                    if (nextField is not null)
+                    {
+                        break;
+                    }
+                }
+
+                if (nextField is null)
+                {
+                    break;
+                }
+
+                _visitedFields.Add((nextField.X, nextField.Y));
+                _roads.Add(nextField);
+
+                int heightDifference = nextField.Height - _gameTable.Table[currentNode.X, currentNode.Y].Height;
+                (double fc, double bc) = CalculateRoadCost(heightDifference);
+                forwardCost += fc;
+                backwardCost += bc;
+
+                Node nextNode = new(nextField.X, nextField.Y, nextField.FieldType);
+
+                if (nextField.FieldType == FieldType.Stop)
+                {
+                    terminatingNode = nextNode;
+                }
+                else if (nextField.FieldType == FieldType.Road && nextField is Road road)
+                {
+                    if (road.RoadType == RoadType.XRoad
+                        || road.RoadType == RoadType.DownTRoad
+                        || road.RoadType == RoadType.UpperTRoad
+                        || road.RoadType == RoadType.LeftTRoad
+                        || road.RoadType == RoadType.RightTRoad)
+                    {
+                        terminatingNode = nextNode;
+                    }
+                }
+
+                currentNode = nextNode;
+            }
+
+            List<Edge> edges = [];
+            List<Walker> newWalkers = [];
+            Node? endNode = null;
+
+            if (terminatingNode is not null)
+            {
+                SharedRoadSequence sharedRoadSequence = new(_roads);
+                edges.Add(new(_startNode, terminatingNode, sharedRoadSequence.ForwardEnumerator(), forwardCost));
+                edges.Add(new(terminatingNode, _startNode, sharedRoadSequence.BackwardEnumerator(), forwardCost));
+
+                foreach ((int dirx, int diry) in directions)
+                {
+                    Field? field = GetNextValidField(dirx, diry);
+                    if (field is null)
+                    {
+                        continue;
+                    }
+                    newWalkers.Add(new Walker(terminatingNode, _gameTable, _visitedFields));
+                }
+
+                endNode = terminatingNode;
+            }
+
+            return new WalkerResult(edges, newWalkers, endNode);
+        }
+        #endregion
+
+        #region Private methods
+        private bool InBounds(int x, int y) => 0 <= x && x < _gameTable.Width && 0 <= y && y < _gameTable.Height;
+
+        private bool IsValidFieldType(FieldType fieldType) => fieldType == FieldType.Road || fieldType == FieldType.Bridge || fieldType == FieldType.Stop;
+
+        private (double forwardCost, double backwardCost) CalculateRoadCost(int heightDifference)
+        {
+            if (heightDifference > 0)
+            {
+                return (2.0, 0.8);
+            }
+            else if (heightDifference < 0)
+            {
+                return (0.8, 2.0);
+            }
+            return (1.0, 1.0);
+        }
+
+        private Field? GetNextValidField(int x, int y)
+        {
+            if (!InBounds(x, y))
+            {
+                return null;
+            }
+            Field nextField = _gameTable.Table[x, y];
+            if (!IsValidFieldType(nextField.FieldType))
+            {
+                return null;
+            }
+            if (_visitedFields.Contains((nextField.X, nextField.Y)))
+            {
+                return null;
+            }
+            return nextField;
+        }
+        #endregion
+    }
+}
