456092624ec3277ccfd73d0ae4f02e3be8757aa8
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-14 13:07:13 +0200

    Added route caching to A* implementation

--- TransportTycoon/TransportTycoon.Model/Graph/AStarPathFinder.cs
+++ TransportTycoon/TransportTycoon.Model/Graph/AStarPathFinder.cs
@@ -1,12 +1,24 @@
 ﻿namespace TransportTycoon.Model.Graph
 {
     /// <summary>
+    /// A record representing a key for caching routes between two nodes in the graph.
+    /// It consists of a start node and an end node.
+    /// </summary>
+    /// <param name="Start">The starting node of the route.</param>
+    /// <param name="End">The ending node of the route.</param>
+    public record RouteKey(Node Start, Node End);
+
+    /// <summary>
     /// A path finder class that implements the A* algorithm to find the shortest path between two nodes in a graph.
     /// </summary>
     public class AStarPathfinder : IPathFinder
     {
         #region Private fields
         private readonly Graph _graph;
+        /// <summary>
+        /// The cache of routes.
+        /// </summary>
+        private readonly Dictionary<RouteKey, List<Edge>> _routeCache = [];
         #endregion
 
         #region Constructors
@@ -22,13 +34,44 @@
 
         #region Public methods
         /// <summary>
-        /// Finds the shortest path from the start node to the end node using the A* algorithm.
+        /// Return the shortest path between the start <see cref="Node"/> and the end <see cref="Node"/> as a list of <see cref="Edge"/> objects.
+        /// Uses caching for already exisiting paths to improve performance.
+        /// If the path is not found in the cache, it runs the A* algorithm to find the path and stores it in the cache before returning it.
+        /// </summary>
+        /// <param name="startNode">The starting node of the path.</param>
+        /// <param name="endNode">The ending node of the path.</param>
+        /// <returns>A list of <see cref="Edge"/> objects representing the shortest path, or <see langword="null"/> if no path is found.</returns>
+        public List<Edge>? FindPath(Node startNode, Node endNode)
+        {
+            RouteKey key = new(startNode, endNode);
+
+            // Try to get the route from the cache first
+            if (_routeCache.TryGetValue(key, out List<Edge>? cachedRoute))
+            {
+                return cachedRoute;
+            }
+
+            // Try the A* algorithm
+            List<Edge>? path = RunAStarAlgorithm(startNode, endNode);
+
+            // If we found a path, cache it for future use
+            if (path is not null)
+            {
+                _routeCache[key] = path;
+            }
+            return path;
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Finds the shortest path from the start <see cref="Node"/> to the end <see cref="Node"/> using the A* algorithm.
         /// The algorithm's pseudocode: <seealso href="https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode"/>
         /// </summary>
         /// <param name="startNode">The starting node.</param>
         /// <param name="endNode">The ending node.</param>
-        /// <returns>A list of edges representing the shortest path from the start node to the end node, or null if no path is found.</returns>
-        public List<Edge>? FindPath(Node startNode, Node endNode)
+        /// <returns>A list of <see cref="Edge"/> objects representing the shortest path from the start <see cref="Node"/> to the end <see cref="Node"/>, or <see langword="null"/> if no path is found.</returns>
+        private List<Edge>? RunAStarAlgorithm(Node startNode, Node endNode)
         {
             PriorityQueue<Node, double> openSet = new();
             Dictionary<Node, Edge> cameFrom = [];
@@ -74,9 +117,7 @@
             }
             return null;
         }
-        #endregion
 
-        #region Private methods
         /// <summary>
         /// Calculates the heuristic (estimated cost) from the current node to the goal node.
         /// </summary>
