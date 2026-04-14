7dc13243308a6d5a7bad842b363d077f8ddbc019
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-14 12:56:25 +0200

    Basic prouth class for handling routes

--- TransportTycoon/TransportTycoon.Model/Prouth.cs
+++ TransportTycoon/TransportTycoon.Model/Prouth.cs
@@ -1,4 +1,41 @@
-﻿namespace TransportTycoon.Model
+﻿using TransportTycoon.Model.Graph;
+
+namespace TransportTycoon.Model
 {
-    public class Prouth;
+    /// <summary>
+    /// A class representing a route between multiple stops in the transport network.
+    /// </summary>
+    public class Prouth
+    {
+        #region Properties
+        /// <summary>
+        /// The list of <see cref="Node"/> objects representing the stops along the route.
+        /// </summary>
+        public List<Node> Stops { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Prouth(List<Node> stops)
+        {
+            Stops = stops;
+        }
+
+        public Prouth()
+        {
+            Stops = [];
+        }
+        #endregion
+
+        #region Public methods
+        public void AddStop(Node stop)
+        {
+            Stops.Add(stop);
+        }
+
+        public void RemoveStop(Node stop)
+        {
+            Stops.Remove(stop);
+        }
+        #endregion
+    }
 }
