e057bb4f1d036b3f4056fc22544c7cb386b7d918
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-04-18 21:14:05 +0200

    Update biomes to work more like enums

--- TransportTycoon/TransportTycoon.MapData/MapGenerator/TerrainGeneration/Biomes.cs
+++ TransportTycoon/TransportTycoon.MapData/MapGenerator/TerrainGeneration/Biomes.cs
@@ -2,35 +2,65 @@
 {
     public static class Biomes
     {
+        #region Public fields
         public static readonly IBiome Default = new DefaultBiome();
         public static readonly IBiome Flat = new FlatBiome();
         public static readonly IBiome Mountainous = new MountainousBiome();
+        #endregion
+
+        #region Private fields
+        private static readonly Dictionary<string, IBiome> _registry = new()
+        {
+            { Default.Id, Default },
+            { Flat.Id, Flat },
+            { Mountainous.Id, Mountainous }
+        };
+        #endregion
+
+        #region Public methods
+        public static IBiome GetById(string id)
+        {
+            if (_registry.TryGetValue(id, out var biome))
+                return biome;
+
+            return Default;
+        }
+        #endregion
     }
 
     internal readonly record struct DefaultBiome : IBiome
     {
-        public float WaterRange => 0.3f;
-        public float PlainRange => 0.55f;
-        public float HillRange => 0.75f;
-        public float MountainRange => 0.95f;
-        public float HighMountainRange => 1f;
+        #region Properties
+        public readonly string Id => nameof(DefaultBiome);
+        public readonly float WaterRange => 0.3f;
+        public readonly float PlainRange => 0.55f;
+        public readonly float HillRange => 0.75f;
+        public readonly float MountainRange => 0.95f;
+        public readonly float HighMountainRange => 1f;
+        #endregion
     }
 
     internal readonly record struct FlatBiome : IBiome
     {
-        public float WaterRange => 0.2f;
-        public float PlainRange => 0.80f;
-        public float HillRange => 0.90f;
-        public float MountainRange => 0.95f;
-        public float HighMountainRange => 1f;
+        #region Properties
+        public readonly string Id => nameof(FlatBiome);
+        public readonly float WaterRange => 0.2f;
+        public readonly float PlainRange => 0.80f;
+        public readonly float HillRange => 0.90f;
+        public readonly float MountainRange => 0.95f;
+        public readonly float HighMountainRange => 1f;
+        #endregion
     }
 
     internal readonly record struct MountainousBiome : IBiome
     {
-        public float WaterRange => 0.2f;
-        public float PlainRange => 0.38f;
-        public float HillRange => 0.70f;
-        public float MountainRange => 0.88f;
-        public float HighMountainRange => 1f;
+        #region Properties
+        public readonly string Id => nameof(MountainousBiome);
+        public readonly float WaterRange => 0.2f;
+        public readonly float PlainRange => 0.38f;
+        public readonly float HillRange => 0.70f;
+        public readonly float MountainRange => 0.88f;
+        public readonly float HighMountainRange => 1f;
+        #endregion
     }
 }
