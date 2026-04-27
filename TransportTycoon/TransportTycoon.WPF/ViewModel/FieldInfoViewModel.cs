80e008e9fd6131dca68e73dbac652ecbe87daa06
Kocsmár Balázs <y3trse@INF.ELTE.HU>
2026-04-27 20:58:37 +0200

    FieldInfoViewModel and TerrainFieldInfoViewModel are created

---
+++ TransportTycoon/TransportTycoon.WPF/ViewModel/FieldInfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportTycoon.WPF.ViewModel
+{
+    public partial class FieldInfoViewModel : ViewModelBase
+    {
+        public string Type { get; init; } = "";
+        public int Height { get; init; }
+        public int X { get; init; }
+        public int Y { get; init; }
+    }
+
+    public class TerrainFieldInfoViewModel : FieldInfoViewModel
+    {
+        public int TreeCount { get; init; }
+    }
+}
