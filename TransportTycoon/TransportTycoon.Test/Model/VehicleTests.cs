30d791e1f85f9009551a74b183287a8c61afafa4
Fényes Ádám <nwxzrb@inf.elte.hu>
2026-05-04 13:42:12 +0200

    Cleaned up and fixed test related to vehicle
    Removed obsolete Direction enum, fixed constructor

--- TransportTycoon/TransportTycoon.Test/Model/VehicleTests.cs
+++ TransportTycoon/TransportTycoon.Test/Model/VehicleTests.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using TransportTycoon.MapData;
+﻿using TransportTycoon.MapData;
 using TransportTycoon.Model;
-using TransportTycoon.Model.Graph;
 
 namespace TransportTycoon.Test.Model
 {
@@ -14,12 +10,12 @@ namespace TransportTycoon.Test.Model
         public void SmallBus_Constructor_SetsCorrectDefaultValues()
         {
             // Arrange & Act
-            var bus = new SmallBus(10, 20, Direction.Up);
+            var bus = new SmallBus(10, 20, 270, null); // Up = 270
 
             // Assert
             Assert.Equal(10, bus.X);
             Assert.Equal(20, bus.Y);
-            Assert.Equal(Direction.Up, bus.Direction);
+            Assert.Equal(270, bus.Angle);
             Assert.Equal(1, bus.TopSpeed);
             Assert.Equal(100, bus.MaxCapacity);
             Assert.Equal(100, bus.Price);
@@ -34,7 +30,7 @@ namespace TransportTycoon.Test.Model
         public void Truck_Constructor_SetsCorrectDefaultValues()
         {
             // Arrange & Act
-            var truck = new Truck(5, 5, Direction.Right);
+            var truck = new Truck(5, 5, 0, null); // Right = 0
 
             // Assert
             Assert.Equal(1.5, truck.TopSpeed);
@@ -49,7 +45,7 @@ namespace TransportTycoon.Test.Model
         public void LiquidTruck_Constructor_SetsCorrectDefaultValues()
         {
             // Arrange & Act
-            var liquidTruck = new LiquidTruck(0, 0, Direction.Down);
+            var liquidTruck = new LiquidTruck(0, 0, 90, null); // Down = 90
 
             // Assert
             Assert.Equal(0.9, liquidTruck.TopSpeed);
@@ -64,7 +60,7 @@ namespace TransportTycoon.Test.Model
         public void SetCurrentCapacity_ValidAmount_UpdatesCapacity()
         {
             // Arrange
-            var van = new Van(0, 0, Direction.Up);
+            var van = new Van(0, 0, 270, null); // Up = 270
 
             // Act
             van.SetCurrentCapacity(50);
@@ -77,7 +73,7 @@ namespace TransportTycoon.Test.Model
         public void SetCurrentCapacity_ExceedsMaxCapacity_DoesNotChangeCapacity()
         {
             // Arrange
-            var van = new Van(0, 0, Direction.Up);
+            var van = new Van(0, 0, 270, null); // Up = 270
             van.SetCurrentCapacity(50); // Beállítunk egy érvényes 50-es értéket
 
             // Act
@@ -91,7 +87,7 @@ namespace TransportTycoon.Test.Model
         public void SetCurrentCapacity_BelowZero_DoesNotChangeCapacity()
         {
             // Arrange
-            var van = new Van(0, 0, Direction.Up);
+            var van = new Van(0, 0, 270, null); // Up = 270
             van.SetCurrentCapacity(50); // Beállítunk egy érvényes 50-es értéket
 
             // Act
@@ -105,7 +101,7 @@ namespace TransportTycoon.Test.Model
         public void SetCurrentLoad_UpdatesLoadCorrectly()
         {
             // Arrange
-            var pickup = new Pickup(0, 0, Direction.Up);
+            var pickup = new Pickup(0, 0, 270, null); // Up = 270
             var wood = new Wood();
 
             // Act
@@ -121,7 +117,7 @@ namespace TransportTycoon.Test.Model
         public void ChangeCurrentSpeed_ValidSpeed_UpdatesSpeed()
         {
             // Arrange
-            var bus = new BigBus(0, 0, Direction.Up);
+            var bus = new BigBus(0, 0, 270, null); // Up = 270
             double validSpeed = bus.TopSpeed / 2;
 
             // Act
@@ -135,7 +131,7 @@ namespace TransportTycoon.Test.Model
         public void ChangeCurrentSpeed_ExceedsTopSpeed_CapsAtTopSpeed()
         {
             // Arrange
-            var bus = new SmallBus(0, 0, Direction.Up);
+            var bus = new SmallBus(0, 0, 270, null); // Up = 270
 
             // Act
             bus.ChangeCurrentSpeed(5.0);
@@ -147,40 +143,13 @@ namespace TransportTycoon.Test.Model
         public void ChangeCurrentSpeed_BelowZero_DoesNotChangeSpeed()
         {
             // Arrange
-            var bus = new SmallBus(0, 0, Direction.Up);
-            double initialSpeed = bus.CurrentSpeed; // A konstruktor 1.0-ra állítja
+            var bus = new SmallBus(0, 0, 270, null); // Up = 270
 
             // Act
-            bus.ChangeCurrentSpeed(-1.0); // Érvénytelen érték, a metódus nem csinál semmit
+            bus.ChangeCurrentSpeed(-1.0);
 
             // Assert
-            Assert.Equal(initialSpeed, bus.CurrentSpeed); // Marad 1.0
-        }
-        #endregion
-
-        #region Útvonal (Prouth) Tesztek
-        [Fact]
-        public void StartDriving_WithValidEdges_SetsCurrentRoute()
-        {
-            // Arrange
-            var van = new Van(0, 0, Direction.Up);
-
-            // Javított Edge és Node konstruktor hívás az Edge.cs és Node.cs alapján
-            var startNode = new Node(0, 0, typeof(Stop));
-            var endNode = new Node(0, 1, typeof(Stop));
-            var dummyRoads = new List<IField>(); // Üres lista az IField-ekhez
-
-            var edges = new List<Edge>
-            {
-                new Edge(startNode, endNode, dummyRoads, 10.0) // Node start, Node end, IEnumerable<IField> roads, double cost
-            };
-
-            // Act
-            van.StartDriving(edges);
-
-            // Assert
-            Assert.NotNull(van.CurrentRoute);
-            Assert.Single(van.CurrentRoute);
+            Assert.Equal(0.0, bus.CurrentSpeed); // Math.Clamp sets to 0.0
         }
         #endregion
     }
