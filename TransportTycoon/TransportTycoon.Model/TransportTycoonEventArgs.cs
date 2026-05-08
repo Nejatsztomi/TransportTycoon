22c2520384ea2adbed0b27fedaa54948a53d7766
Kis Tamás <iscbst@INF.ELTE.HU>
2026-05-08 22:54:56 +0200

    Added TransportTycoonEventArgs summary

--- TransportTycoon/TransportTycoon.Model/TransportTycoonEventArgs.cs
+++ TransportTycoon/TransportTycoon.Model/TransportTycoonEventArgs.cs
@@ -9,10 +9,21 @@
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Gets the current game time in milliseconds since the game started.
+        /// </summary>
+        /// <remarks>This property provides the elapsed time for the game, which can be used for timing
+        /// events or animations. The value is updated continuously as the game runs.</remarks>
         public ulong GameTime => _gameTime;
 
+        /// <summary>
+        /// Gets the current maintenance value, representing the maintenance status of the system.
+        /// </summary>
         public int Maintenance => _maintenance;
 
+        /// <summary>
+        /// Returns the players's vehicle numbers
+        /// </summary>
         public int NumberOfVehicles => _numberOfVehicles;
         #endregion
 
